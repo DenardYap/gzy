8147d10725b61fae71189462b7963b0468cdb5c7
DenardYap <mameehy@hotmail.com>
2022-06-30 01:40:58 +0800

    disabled webpack5, see if it fixes the issue

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack5: false,
 };
 
 module.exports = {
