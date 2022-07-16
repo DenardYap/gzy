7c47f7c7476553dc58e9f7ec1975aa04096fceb1
DenardYap <mameehy@hotmail.com>
2022-07-16 21:18:56 +0800

    switched from nodemaielr to AWS SES, I am hoping this solves the email problem

--- pages/api/webhook/index.js
+++ pages/api/webhook/index.js
@@ -226,8 +226,8 @@ export default async function handler(req, res) {
       buf,
       sig,
       // process.env.NEXT_PUBLIC_END_POINT_SECRET
-      // process.env.NEXT_PUBLIC_END_POINT_SECRET_TEST
-      process.env.NEXT_PUBLIC_END_POINT_SECRET_LOCAL
+      process.env.NEXT_PUBLIC_END_POINT_SECRET_TEST
+      // process.env.NEXT_PUBLIC_END_POINT_SECRET_LOCAL
     );
   } catch (err) {
     console.log("Error!", err.message);
