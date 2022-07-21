8433c2194d185fc574e9febdf5696d80de5b64c0
DenardYap <mameehy@hotmail.com>
2022-07-21 13:16:27 +0800

    asdasd

--- pages/api/upload/getURL.js
+++ pages/api/upload/getURL.js
@@ -54,7 +54,11 @@ export default async function handler(req, res) {
           CacheControl: "no-cache",
         };
         // console.log(req.body);
-        const uploadedImage = await s3.putObject(params).promise();
+        const uploadedImage = await s3
+          .putObject(params, (err, data) => {
+            if (err) rej(err);
+          })
+          .promise();
         // putObject too
         console.log(
           `Successfully updated main_aimg${req.query.num}.jpg at ${uploadedImage.Location}`
@@ -65,13 +69,35 @@ export default async function handler(req, res) {
     });
   };
   // const filepath = await formPromised(req);
-  formPromised(req)
-    .then((filepath) => {
-      res.status(200).json({ filepath });
-    })
-    .catch((err) => {
-      res.status(404).json({ error: err });
-    });
+  // formPromised(req)
+  //   .then((filepath) => {
+  //     res.status(200).json({ filepath });
+  //   })
+  //   .catch((err) => {
+  //     res.status(404).json({ error: err });
+  //   });
+
+  const region = "ap-southeast-1";
+  const bucketName = "guanzhiyan";
+  const accessKeyId = process.env.ACCESS_KEY_ID;
+  const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+  const s3 = new aws.S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+    signatureVersion: "v4",
+  });
+  const params = {
+    Bucket: bucketName,
+    Key: `main_aimg${req.query.num}.jpg`,
+    // Key: `last_testing${req.query.num}.jpg`,
+    Expires: new Date(),
+    Body,
+    CacheControl: "no-cache",
+  };
+  // console.log(req.body);
+  const uploadedImage = await s3.putObject(params).promise();
+
   // console.log("promise successful");
   // return res.status(200).json({ filepath });
   // .then((data) => {
