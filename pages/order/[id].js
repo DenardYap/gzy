738491a44fa95d75471e39354d0725dc494c6a47
DenardYap <mameehy@hotmail.com>
2022-07-25 18:35:17 +0800

    can't get on-demand revalidation to work on AWS, for now, just using the revalidation while I am fixing this

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -17,10 +17,7 @@ import styles from "../../styles/OrderItem.module.css";
 import { useRouter } from "next/router";
 const Order = (props) => {
   const router = useRouter();
-
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
+  const [renderWait, setRenderWait] = useState(true);
   const [dynamicID, setDynamicID] = useContext(dynamicContext);
   const { t } = useTranslation("common");
   const language = useContext(languageContext);
@@ -72,6 +69,11 @@ const Order = (props) => {
     })();
   }, []);
 
+  useEffect(() => {
+    if (!router.isFallback) {
+      setRenderWait(false);
+    }
+  });
   function formatDate() {
     var hours = date.getHours();
     var minutes = date.getMinutes();
@@ -93,181 +95,163 @@ const Order = (props) => {
   }
   return (
     <div>
-      <div className="mt-10 mb-2 mx-[1em] underline">
-        {date ? <h2> Order placed on {formatDate()}</h2> : <></>}
-      </div>
-      <div className="flex flex-col rounded bg-slate-200 text-gray-400 p-5 mx-[1em] mb-10 min-h-[75vh] ">
-        <h2 className="text-4xl  mb-3   text-slate-800">
-          {t("delivery_status")}
-        </h2>
-        <div className="flex flex-row justify-around text-9xl bg-slate-50 p-5 rounded shadow-2xl">
-          <div className="flex flex-col  items-center text-center">
-            <FaShoppingCart className="text-green-400   "></FaShoppingCart>
-            <h2 className="text-xl text-green-400 font-semibold">
-              {t("order_confirm")}
+      {renderWait ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div className="mt-10 mb-2 mx-[1em] underline">
+            {date ? <h2> Order placed on {formatDate()}</h2> : <></>}
+          </div>
+          <div className="flex flex-col rounded bg-slate-200 text-gray-400 p-5 mx-[1em] mb-10 min-h-[75vh] ">
+            <h2 className="text-4xl  mb-3   text-slate-800">
+              {t("delivery_status")}
             </h2>
-          </div>
-          {status >= 2 ? (
-            <AiOutlineMinus className="text-green-400"></AiOutlineMinus>
-          ) : (
-            <AiOutlineEllipsis></AiOutlineEllipsis>
-          )}
-
-          <div className="flex flex-col items-center text-center">
-            {status >= 2 ? (
-              <>
-                <FiPackage className="text-green-400   "></FiPackage>
+            <div className="flex flex-row justify-around text-9xl bg-slate-50 p-5 rounded shadow-2xl">
+              <div className="flex flex-col  items-center text-center">
+                <FaShoppingCart className="text-green-400   "></FaShoppingCart>
                 <h2 className="text-xl text-green-400 font-semibold">
-                  {t("packaged")}
+                  {t("order_confirm")}
                 </h2>
+              </div>
+              {status >= 2 ? (
+                <AiOutlineMinus className="text-green-400"></AiOutlineMinus>
+              ) : (
+                <AiOutlineEllipsis></AiOutlineEllipsis>
+              )}
+
+              <div className="flex flex-col items-center text-center">
+                {status >= 2 ? (
+                  <>
+                    <FiPackage className="text-green-400   "></FiPackage>
+                    <h2 className="text-xl text-green-400 font-semibold">
+                      {t("packaged")}
+                    </h2>
+                  </>
+                ) : (
+                  <>
+                    <FiPackage></FiPackage>
+                    <h2 className="text-xl  font-semibold">{t("packaged")}</h2>
+                  </>
+                )}
+              </div>
+              {status >= 3 ? (
+                <AiOutlineMinus className="text-green-400"></AiOutlineMinus>
+              ) : (
+                <AiOutlineEllipsis></AiOutlineEllipsis>
+              )}
+
+              <div className="flex flex-col items-center text-center">
+                {status >= 3 ? (
+                  <>
+                    <MdLocalShipping className="text-green-400   "></MdLocalShipping>
+                    <h2 className="text-xl text-green-400 font-semibold">
+                      {t("shipped")}
+                    </h2>
+                  </>
+                ) : (
+                  <>
+                    <MdLocalShipping></MdLocalShipping>
+                    <h2 className="text-xl  font-semibold">{t("shipped")}</h2>
+                  </>
+                )}
+              </div>
+
+              {status >= 4 ? (
+                <AiOutlineMinus className="text-green-400"></AiOutlineMinus>
+              ) : (
+                <AiOutlineEllipsis></AiOutlineEllipsis>
+              )}
+              <div className="flex flex-col items-center text-center">
+                {status >= 4 ? (
+                  <>
+                    <BiHomeSmile className="text-green-400   "></BiHomeSmile>
+                    <h2 className="text-xl text-green-400 font-semibold">
+                      {t("delivered")}
+                    </h2>
+                  </>
+                ) : (
+                  <>
+                    <BiHomeSmile></BiHomeSmile>
+                    <h2 className="text-xl  font-semibold">{t("delivered")}</h2>
+                  </>
+                )}
+              </div>
+            </div>
+
+            <h2 className="text-4xl underline  mb-3 my-10  text-slate-800">
+              {t("your_orders")}
+            </h2>
+            <div className="flex flex-col">
+              <div className={styles.topGrid}>
+                <h3>{t("Image")}</h3>
+                <h3>{t("Name")}</h3>
+                <h3>{t("Quantity")}</h3>
+                <h3>{t("Price")}</h3>
+                <h3>{t("Subtotal")}</h3>
+              </div>
+              <div className="flex flex-col justify-around text-xl bg-slate-50 text-slate-800 shadow-2xl">
+                {data == null ? (
+                  <></>
+                ) : (
+                  data.items_bought.map((item) => (
+                    <OrderItem data={item} key={item.id}></OrderItem>
+                  ))
+                )}
+              </div>
+              <h2 className="w-[100%]  bg-slate-800 text-slate-50 text-center">
+                {t("total_text1")}:{" "}
+                {data ? <>RM{parseInt(data.amount).toFixed(2)}</> : <>...</>}
+              </h2>
+            </div>
+            <h2 className="text-4xl underline  mb-3 my-10  text-slate-800">
+              {t("customer_details")}
+            </h2>
+            {data ? (
+              <>
+                <div
+                  className={`${styles.customerGrid}  justify-around text-xl bg-slate-800 text-slate-50 `}
+                >
+                  <h2>{t("Name")}</h2>
+                  <h2>{t("Phone")}</h2>
+                  <h2>{t("Email")}</h2>
+                  <h2>{t("Address1")}</h2>
+                  <h2>{t("Address2")}</h2>
+                  <h2>{t("Postal")} Code</h2>
+                  <h2>{t("City")}</h2>
+                  <h2>{t("State")}</h2>
+                  <h2>{t("Country")}</h2>
+                </div>
+                <div
+                  className={`${styles.customerGrid}  justify-around text-xl bg-slate-50 text-slate-800 rounded shadow-2xl`}
+                >
+                  <h2>{data.name}</h2>
+                  <h2>{data.phone}</h2>
+                  <h2>{data.email}</h2>
+                  <h2>{data.line_1}</h2>
+                  <h2>{data.line_2}</h2>
+                  <h2>{data.postal_code}</h2>
+                  <h2>{data.city}</h2>
+                  <h2>{data.state}</h2>
+                  <h2>{data.country}</h2>
+                </div>
               </>
             ) : (
-              <>
-                <FiPackage></FiPackage>
-                <h2 className="text-xl  font-semibold">{t("packaged")}</h2>
-              </>
-            )}
-          </div>
-          {status >= 3 ? (
-            <AiOutlineMinus className="text-green-400"></AiOutlineMinus>
-          ) : (
-            <AiOutlineEllipsis></AiOutlineEllipsis>
-          )}
-
-          <div className="flex flex-col items-center text-center">
-            {status >= 3 ? (
-              <>
-                <MdLocalShipping className="text-green-400   "></MdLocalShipping>
-                <h2 className="text-xl text-green-400 font-semibold">
-                  {t("shipped")}
-                </h2>
-              </>
-            ) : (
-              <>
-                <MdLocalShipping></MdLocalShipping>
-                <h2 className="text-xl  font-semibold">{t("shipped")}</h2>
-              </>
-            )}
-          </div>
-
-          {status >= 4 ? (
-            <AiOutlineMinus className="text-green-400"></AiOutlineMinus>
-          ) : (
-            <AiOutlineEllipsis></AiOutlineEllipsis>
-          )}
-          <div className="flex flex-col items-center text-center">
-            {status >= 4 ? (
-              <>
-                <BiHomeSmile className="text-green-400   "></BiHomeSmile>
-                <h2 className="text-xl text-green-400 font-semibold">
-                  {t("delivered")}
-                </h2>
-              </>
-            ) : (
-              <>
-                <BiHomeSmile></BiHomeSmile>
-                <h2 className="text-xl  font-semibold">{t("delivered")}</h2>
-              </>
-            )}
-          </div>
-        </div>
-
-        <h2 className="text-4xl underline  mb-3 my-10  text-slate-800">
-          {t("your_orders")}
-        </h2>
-        <div className="flex flex-col">
-          <div className={styles.topGrid}>
-            <h3>{t("Image")}</h3>
-            <h3>{t("Name")}</h3>
-            <h3>{t("Quantity")}</h3>
-            <h3>{t("Price")}</h3>
-            <h3>{t("Subtotal")}</h3>
-          </div>
-          <div className="flex flex-col justify-around text-xl bg-slate-50 text-slate-800 shadow-2xl">
-            {data == null ? (
               <></>
-            ) : (
-              data.items_bought.map((item) => (
-                <OrderItem data={item} key={item.id}></OrderItem>
-              ))
             )}
           </div>
-          <h2 className="w-[100%]  bg-slate-800 text-slate-50 text-center">
-            {t("total_text1")}:{" "}
-            {data ? <>RM{parseInt(data.amount).toFixed(2)}</> : <>...</>}
-          </h2>
-        </div>
-        <h2 className="text-4xl underline  mb-3 my-10  text-slate-800">
-          {t("customer_details")}
-        </h2>
-        {data ? (
-          <>
-            <div
-              className={`${styles.customerGrid}  justify-around text-xl bg-slate-800 text-slate-50 `}
-            >
-              <h2>{t("Name")}</h2>
-              <h2>{t("Phone")}</h2>
-              <h2>{t("Email")}</h2>
-              <h2>{t("Address1")}</h2>
-              <h2>{t("Address2")}</h2>
-              <h2>{t("Postal")} Code</h2>
-              <h2>{t("City")}</h2>
-              <h2>{t("State")}</h2>
-              <h2>{t("Country")}</h2>
-            </div>
-            <div
-              className={`${styles.customerGrid}  justify-around text-xl bg-slate-50 text-slate-800 rounded shadow-2xl`}
-            >
-              <h2>{data.name}</h2>
-              <h2>{data.phone}</h2>
-              <h2>{data.email}</h2>
-              <h2>{data.line_1}</h2>
-              <h2>{data.line_2}</h2>
-              <h2>{data.postal_code}</h2>
-              <h2>{data.city}</h2>
-              <h2>{data.state}</h2>
-              <h2>{data.country}</h2>
-            </div>
-          </>
-        ) : (
-          <></>
-        )}
-      </div>
+        </>
+      )}
     </div>
   );
 };
 
 export default Order;
 
-export async function getStaticProps({ params, locale }) {
-  const ObjectId = require("mongodb").ObjectId;
-  const { db } = await connectToDatabase();
-  let data = await db
-    .collection("tracking")
-    .find({ _id: params.id.toString() })
-    .toArray();
-  data = JSON.parse(JSON.stringify(data));
-
-  if (data.length == 0) {
-    console.log("item doesn't exist");
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ["common"], nextI18nextConfig)),
-      data,
-      id: params.id,
-    },
-    revalidate: 5,
-    // revalidate: 300,
-  };
-}
 export async function getStaticPaths({ locales }) {
   const { db } = await connectToDatabase();
   let data = await db.collection("tracking").find().toArray();
 
+  console.log("here3");
   // can use cached data here...
   data = JSON.parse(JSON.stringify(data));
 
@@ -282,8 +266,36 @@ export async function getStaticPaths({ locales }) {
     )
     .flat(); // Flatten array to avoid nested arrays
 
+  console.log("here4");
   return {
     paths,
     fallback: true,
+  };
+}
+
+export async function getStaticProps({ params, locale }) {
+  const ObjectId = require("mongodb").ObjectId;
+  const { db } = await connectToDatabase();
+  let data = await db
+    .collection("tracking")
+    .find({ _id: params.id.toString() })
+    .toArray();
+  data = JSON.parse(JSON.stringify(data));
+  console.log("here1");
+  if (data.length == 0) {
+    console.log("item doesn't exist");
+    return {
+      notFound: true,
+    };
+  }
+  console.log("here2");
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"], nextI18nextConfig)),
+      data,
+      id: params.id,
+    },
+    revalidate: 5,
+    // revalidate: 300,
   };
 }
